6f0b6a444ff276f5efa699854474cc0e86d1afda
Stefan Alfbo <stefan@alfbo.se>
2022-12-11 15:56:58 +0100

    Add first attepmt to solve part two of day two

--- 2022/src/day02.test.ts
+++ 2022/src/day02.test.ts
@@ -25,9 +25,9 @@ describe('parseEncryptedStrategyGuide with the example input', () => {
 describe('The Round score', () => {
 
     it('should be 3 + hand when equal', async () => {
-        const paperRound = new Round(Hand.Paper, Hand.Paper)
-        const rockRound = new Round(Hand.Rock, Hand.Rock)
-        const scissorsRound = new Round(Hand.Scissors, Hand.Scissors)
+        const paperRound = new Round(Hand.Paper, Hand.Paper, "Y")
+        const rockRound = new Round(Hand.Rock, Hand.Rock, "X")
+        const scissorsRound = new Round(Hand.Scissors, Hand.Scissors, "Z")
 
         expect(paperRound.score()).toBe(3 + Hand.Paper)
         expect(rockRound.score()).toBe(3 + Hand.Rock)
@@ -35,9 +35,9 @@ describe('The Round score', () => {
     })
 
     it('should be 6 + hand when I win', async () => {
-        const scissorsWin = new Round(Hand.Paper, Hand.Scissors)
-        const paperWin = new Round(Hand.Rock, Hand.Paper)
-        const rockWin = new Round(Hand.Scissors, Hand.Rock)
+        const scissorsWin = new Round(Hand.Paper, Hand.Scissors, "Z")
+        const paperWin = new Round(Hand.Rock, Hand.Paper, "Y")
+        const rockWin = new Round(Hand.Scissors, Hand.Rock, "X")
 
         expect(scissorsWin.score()).toBe(6 + Hand.Scissors)
         expect(paperWin.score()).toBe(6 + Hand.Paper)
@@ -45,9 +45,9 @@ describe('The Round score', () => {
     })
 
     it('should be hand when I lose', async () => {
-        const scissorsLose = new Round(Hand.Rock, Hand.Scissors)
-        const paperLose = new Round(Hand.Scissors, Hand.Paper)
-        const rockLose = new Round(Hand.Paper, Hand.Rock)
+        const scissorsLose = new Round(Hand.Rock, Hand.Scissors, "Z")
+        const paperLose = new Round(Hand.Scissors, Hand.Paper, "Y")
+        const rockLose = new Round(Hand.Paper, Hand.Rock, "X")
 
         expect(scissorsLose.score()).toBe(Hand.Scissors)
         expect(paperLose.score()).toBe(Hand.Paper)
@@ -55,13 +55,55 @@ describe('The Round score', () => {
     })
 })
 
+describe('The Round score_part_two', () => {
+    const _not_important = Hand.Paper
+
+    it('should be 3 + hand when equal', async () => {
+        const paperRound = new Round(Hand.Paper, _not_important, "Y")
+        const rockRound = new Round(Hand.Rock, _not_important, "Y")
+        const scissorsRound = new Round(Hand.Scissors, _not_important, "Y")
+
+        expect(paperRound.score_part_two()).toBe(3 + Hand.Paper)
+        expect(rockRound.score_part_two()).toBe(3 + Hand.Rock)
+        expect(scissorsRound.score_part_two()).toBe(3 + Hand.Scissors)
+    })
+
+    it('should be 6 + hand when I win', async () => {
+        const scissorsWin = new Round(Hand.Paper, _not_important, "Z")
+        const paperWin = new Round(Hand.Rock, _not_important, "Z")
+        const rockWin = new Round(Hand.Scissors, _not_important, "Z")
+
+        expect(scissorsWin.score_part_two()).toBe(6 + Hand.Scissors)
+        expect(paperWin.score_part_two()).toBe(6 + Hand.Paper)
+        expect(rockWin.score_part_two()).toBe(6 + Hand.Rock)
+    })
+
+    it('should be hand when I lose', async () => {
+        const scissorsLose = new Round(Hand.Rock, _not_important, "X")
+        const paperLose = new Round(Hand.Scissors, _not_important, "X")
+        const rockLose = new Round(Hand.Paper, _not_important, "X")
+
+        expect(scissorsLose.score_part_two()).toBe(Hand.Scissors)
+        expect(paperLose.score_part_two()).toBe(Hand.Paper)
+        expect(rockLose.score_part_two()).toBe(Hand.Rock)
+    })
+})
+
 describe('Example', () => {
 
-    it('should be 15', async () => {
+    it('should be 15 for part one', async () => {
         const rounds = parseEncryptedStrategyGuide(fileContent)
 
         const sum = rounds.reduce((sum, round) => sum + round.score(), 0)
 
         expect(sum).toBe(15)
+    })
+
+    it('should be 12 for part two', async () => {
+        const rounds = parseEncryptedStrategyGuide(fileContent)
+
+        const sum = rounds.reduce((sum, round) => sum + round.score_part_two(), 0)
+
+        expect(sum).toBe(12)
     })
 })
--- 2022/src/day02.ts
+++ 2022/src/day02.ts
@@ -5,7 +5,7 @@ const inputFile: string = 'day02-input.txt'
 const newLine = "\n"
 
 type Opponent = "A" | "B" | "C"
-type Me = "X" | "Y" | "Z"
+type Strategy = "X" | "Y" | "Z"
 export enum Hand {
     Rock = 1,
     Paper = 2,
@@ -21,7 +21,7 @@ const opponentHand = (opponent: Opponent): Hand => {
     }
 }
 
-const meHand = (me: Me): Hand => {
+const meHand = (me: Strategy): Hand => {
     switch (me) {
         case "X": return Hand.Rock
         case "Y": return Hand.Paper
@@ -32,10 +32,12 @@ const meHand = (me: Me): Hand => {
 export class Round {
     readonly opponent: Hand
     readonly me: Hand
+    readonly strategy: Strategy
 
-    constructor(opponent: Hand, me: Hand) {
+    constructor(opponent: Hand, me: Hand, strategy: Strategy) {
         this.opponent = opponent
         this.me = me
+        this.strategy = strategy
     }
 
     score(): Score {
@@ -57,6 +59,31 @@ export class Round {
 
         return LOSS
     }
+
+    score_part_two(): Score {
+        const EQUAL = 3
+        const WIN = 6
+
+        if (this.strategy == "Y")
+            return this.opponent + EQUAL
+
+        if (this.strategy == "Z" && this.opponent == Hand.Scissors)
+            return Hand.Rock + WIN
+
+        if (this.strategy == "Z" && this.opponent == Hand.Paper)
+            return Hand.Scissors + WIN
+
+        if (this.strategy == "Z" && this.opponent == Hand.Rock)
+            return Hand.Paper + WIN
+
+        if (this.strategy == "X" && this.opponent == Hand.Scissors)
+            return Hand.Paper
+
+        if (this.strategy == "X" && this.opponent == Hand.Paper)
+            return Hand.Rock
+
+        return Hand.Scissors
+    }
 }
 
 export const parseEncryptedStrategyGuide = (content: string) => {
@@ -64,9 +91,11 @@ export const parseEncryptedStrategyGuide = (content: string) => {
         .split(newLine)
         .map(line => {
             const handShapes = line.split(" ")
+            const strategy = handShapes[1] as Strategy
             return new Round(
                 opponentHand(handShapes[0] as Opponent),
-                meHand(handShapes[1] as Me)
+                meHand(strategy),
+                strategy
             )
         })
 
@@ -75,9 +104,16 @@ export const parseEncryptedStrategyGuide = (content: string) => {
 export const solution = () => {
     const content = readFileSync(join(__dirname, inputFile), 'utf-8')
 
-    const totalScore = parseEncryptedStrategyGuide(content)
-        .reduce((sum, round) => sum + round.score(), 0)
+    const rounds = parseEncryptedStrategyGuide(content)
+
+    // const totalScore = parseEncryptedStrategyGuide(content)
+    //     .reduce((sum, round) => sum + round.score(), 0)
 
     // Answer to part 1
+    const totalScore = rounds.reduce((sum, round) => sum + round.score(), 0)
     console.log(`My total score according to the strategy guide is ${totalScore}`)
+
+    // Answer to part 2
+    const totalScorePartTwo = rounds.reduce((sum, round) => sum + round.score_part_two(), 0)
+    console.log(`My total score according exactly to the strategy guid is ${totalScorePartTwo}`)
 }
